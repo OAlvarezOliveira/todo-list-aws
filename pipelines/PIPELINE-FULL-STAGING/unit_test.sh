a3904cf440970756d39889ad86c795ea142aab5f
Óscar Álvarez <oalvarezoliveira@gmail.com>
2023-05-27 17:32:33 +0200

    Comentar SSH

--- pipelines/PIPELINE-FULL-STAGING/unit_test.sh
+++ pipelines/PIPELINE-FULL-STAGING/unit_test.sh
@@ -19,11 +19,5 @@ python test/unit/TestToDo.py
 # Mostrar información de cobertura
 pip show coverage
 coverage run --include=src/todoList.py test/unit/TestToDo.py
-coverage report -m
-coverage xml
-
-# Generar informe XML compatible con Cobertura
-coverage xml -o coverage.xml
-
-# Analizar el informe XML de Cobertura para mostrar las líneas no cubiertas
 coverage report --show-missing
+coverage xml
