82cd8d00cb5384725aa8754b7e3178db679d0769
Óscar Álvarez <oalvarezoliveira@gmail.com>
2023-05-27 16:38:44 +0200

    Generar informe XML compatible con Cobertura Analizar el informe XML de Cobertura para mostrar las líneas no cubiertas

--- pipelines/PIPELINE-FULL-STAGING/unit_test.sh
+++ pipelines/PIPELINE-FULL-STAGING/unit_test.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
+# Activar entorno virtual
 source todo-list-aws/bin/activate
+
+# Habilitar modo detallado
 set -x
+
+# Establecer PYTHONPATH
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 echo "PYTHONPATH: $PYTHONPATH"
+
+# Establecer variable de entorno para la tabla de DynamoDB
 export DYNAMODB_TABLE=todoUnitTestsTable
+
+# Ejecutar pruebas unitarias con Python
 python test/unit/TestToDo.py
+
+# Mostrar información de cobertura
 pip show coverage
 coverage run --include=src/todoList.py test/unit/TestToDo.py
-coverage report
+coverage report -m
 coverage xml
+
+# Generar informe XML compatible con Cobertura
+coverage xml -o coverage.xml
+
+# Analizar el informe XML de Cobertura para mostrar las líneas no cubiertas
+cobertura-coverage.sh report --xml=coverage.xml --uncovered-only
