f70829a1242d033aea80c7b8516e0104609f0d4e
Óscar Álvarez <oalvarezoliveira@gmail.com>
2023-06-04 10:12:11 +0200

    corregir Flake8 estilos

--- src/translate.py
+++ src/translate.py
@@ -2,20 +2,21 @@ import json
 import decimalencoder
 import todoList
 
-
 def translate(event, context):
-    # Obtener el elemento de la lista de tareas mediante el ID proporcionado en los parámetros de la ruta
+    # Obtener elemento todoList mediante el ID proporcionado pathParameters
     item = todoList.get_item(event['pathParameters']['id'])
-    print(json.dumps(event['pathParameters'], indent=4))  # Imprimir los parámetros de la ruta en formato JSON
+    # Imprimir los parámetros de la ruta en formato JSON
+    print(json.dumps(event['pathParameters'], indent=4))  
     
-    # Traducir el texto del elemento al idioma especificado en los parámetros de la ruta
+    # Traducir el texto del elemento al idioma especificado pathParameters
     idioma = event['pathParameters']['idioma']
     traduccion = str(todoList.get_translate(item["text"], idioma))
     
-    # Actualizar el texto del elemento con la traducción obtenida
+    # Actualizar el texto del elemento con la traducción 
     item["text"] = traduccion
     print("Traducción del texto:\n\r")
-    print(json.dumps(item["text"], cls=decimalencoder.DecimalEncoder, indent=4))  # Imprimir la traducción en formato JSON
+    # Imprimir la traducción en formato JSON
+    print(json.dumps(item["text"], cls=decimalencoder.DecimalEncoder, indent=4))
     
     # Crear la respuesta según si se encontró o no el elemento
     if item:
