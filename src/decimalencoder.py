0d0afcdd7d0eb4b17d05d0a6aa59fab9fbe6c5eb
Óscar Álvarez <oalvarezoliveira@gmail.com>
2023-06-07 18:07:29 +0200

    Test Traslate

--- src/decimalencoder.py
+++ src/decimalencoder.py
@@ -2,7 +2,6 @@ import decimal
 import json
 
 
-# This is a workaround for: http://bugs.python.org/issue16535
 class DecimalEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, decimal.Decimal):
